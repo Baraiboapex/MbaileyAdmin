bd7dfe66f9d4a4bee492f97493c6694d8fe6bc4b
matth <matth@BARAIBOBOX>
2024-10-16 12:23:52 -0400

    init

---
+++ MbaileyAdmin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MbaileyAdmin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
